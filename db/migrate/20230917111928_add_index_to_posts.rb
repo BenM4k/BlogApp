4659b671c9168f90777d325ccb9deb6d79ce8318
BenM4k <bennymakomo@gmail.com>
2023-09-17 18:37:03 +0200

    correct migrations

--- db/migrate/20230917111928_add_index_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToPosts < ActiveRecord::Migration[7.0]
-  def change
-    add_index :posts, :author_id, unique: true
-  end
-end
--- db/migrate/20230917112156_add_index_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToComments < ActiveRecord::Migration[7.0]
-  def change
-    add_index :comments, :user_id, unique: true
-  end
-end
