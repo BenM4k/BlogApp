d17b9b26e0fbc548b27840b963fe15d179bb7cd8
BenM4k <bennymakomo@gmail.com>
2023-09-25 18:24:25 +0200

    views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: %i[ show edit update destroy ]
+    def index
+        @users = User.all
+    end
+
+    def show
+    end
+
+    private
+    
+    def set_user
+        @user = User.find(params[:id])
+        @posts = Post.where(author_id: @user.id)
+    end
+end
