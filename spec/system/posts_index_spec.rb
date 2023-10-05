9f743afcecf648d970ad447652eb2a198e568fd8
BenM4k <bennymakomo@gmail.com>
2023-10-05 17:04:55 +0200

    specs for users_paths

--- spec/system/posts_index_spec.rb
+++ spec/system/posts_index_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
-describe "a specification" do
-    before(:all) do
-        Comment.delete_all
-        Post.delete_all
-        User.delete_all
-        @tom = User.create(name: 'Tom', photo: 'https://placehold.co/200x133', bio: 'Teacher from Mexico.',
-                           posts_counter: 0)
-        @first_post = Post.create(author: @tom, title: 'Hello', text: 'This is my first post', comments_counter: 0,
-                                  likes_counter: 0)
+describe 'a specification' do
+  before(:all) do
+    Comment.delete_all
+    Post.delete_all
+    User.delete_all
+    @tom = User.create(name: 'Tom', photo: 'https://placehold.co/200x133', bio: 'Teacher from Mexico.',
+                       posts_counter: 0)
+    @first_post = Post.create(author: @tom, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+                              likes_counter: 0)
+  end
 
-    end
+  it 'has users posts' do
+    visit "/users/#{@tom.id}/posts"
+    expect(page).to have_content("#{@tom.name}'s posts page")
+  end
 
-    it "has users posts" do
-        visit "/users/#{@tom.id}/posts"
-        expect(page).to have_content("#{@tom.name}'s posts page")
-    end
-
-    it "has users posts" do
-        visit "/users/#{@tom.id}/posts"
-        click_link 'show post'
-        expect(page).to have_content("#{@first_post.title}")
-    end
+  it 'has users posts' do
+    visit "/users/#{@tom.id}/posts"
+    click_link 'show post'
+    expect(page).to have_content(@first_post.title.to_s)
+  end
 end
